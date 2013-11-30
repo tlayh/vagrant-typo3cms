14709351d016ff73a476f1edbd2dddafe64a07e8
Thomas Layh <develop@layh.com>
2013-11-30 15:53:20 +0100

    remove bind _address for mysql to allow external connections

--- site-cookbooks/typo3-cms/attributes/default.rb
+++ site-cookbooks/typo3-cms/attributes/default.rb
@@ -2,3 +2,4 @@ include_attribute "mysql::server"
 node.set[:mysql][:server_root_password] = 'root'
 node['mysql']['server_debian_password'] = 'root'
 node['mysql']['server_repl_password'] = 'root'
+node['mysql']['bind_address'] = ''
